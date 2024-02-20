5059c179e5e8b7134ceec02827419601479bf3c0
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2024-02-20 19:17:18 +0100

    Better defaults in argsparser

--- powheg_tools/cleanpowheg/__init__.py
+++ powheg_tools/cleanpowheg/__init__.py
@@ -7,12 +7,11 @@ def main():
     parser = argparse.ArgumentParser(description="Clean up powheg directory")
 
     # Adding optional argument with default value
-    parser.add_argument("path", nargs="?", type=str, help="path to powheg directory")
+    parser.add_argument(
+        "--path", default=".", type=str, help="path to powheg directory"
+    )
 
     args = parser.parse_args()
-
-    # Accessing optional argument
-    path = args.path if args.path else "."
 
     # remove single file
     for f in [
@@ -27,14 +26,14 @@ def main():
         "pwghistnorms.top",
         "pwgxgrid.dat",
         "realequivregions-rad",
-        "parameters.ol",
         "pwgboundviolations.dat",
         "pwgevents.lhe",
         "pwg-stat.dat",
         "pwhg_checklimits",
         "virtequiv",
+        "parameters.ol",
     ]:
-        p = os.path.join(path, f)
+        p = os.path.join(args.path, f)
         if os.path.exists(p):
             os.remove(p)
     # remove parallel run files
--- powheg_tools/genpwgseeds/__init__.py
+++ powheg_tools/genpwgseeds/__init__.py
@@ -7,10 +7,12 @@ def main():
     parser = argparse.ArgumentParser(description="Generate powheg seeds")
 
     # Adding optional argument with default value
-    parser.add_argument("n", type=int, default=2000, help="number of seeds")
-    parser.add_argument("start", type=int, default=1, help="start value")
-    parser.add_argument("end", type=int, default=32768, help="end value")
-    parser.add_argument("output", type=str, default="pwgseeds.dat", help="output file")
+    parser.add_argument("--n", type=int, default=2000, help="number of seeds")
+    parser.add_argument("--start", type=int, default=1, help="start value")
+    parser.add_argument("--end", type=int, default=32768, help="end value")
+    parser.add_argument(
+        "--output", type=str, default="pwgseeds.dat", help="output file"
+    )
 
     args = parser.parse_args()
 
