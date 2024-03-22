5f6cdddac118d41a92d76205874c18efb693a617
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2024-03-22 14:38:47 +0100

    Add parallelrun cleanup

--- powheg_tools/cleanpowheg/__init__.py
+++ powheg_tools/cleanpowheg/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import argparse
+import glob
 import os
+from pathlib import Path
 
 
 def main():
@@ -33,12 +35,53 @@ def main():
         "pwhg_checklimits",
         "virtequiv",
         "parameters.ol",
+        "mint_upb_rmupb.top",
+        "mint_upb_btlupb.top",
+        "mint_upb_btlupb_rat.top",
     ]:
         p = os.path.join(args.path, f)
         if os.path.exists(p):
             os.remove(p)
     # remove parallel run files
-    # TODO
+    for n, s in [
+        ("pwggrid-rm-", ".dat"),
+        ("pwggrid-btl-", ".dat"),
+        ("pwgrmupb-", ".dat"),
+        ("pwgbtlupb-", ".dat"),
+        ("pwgboundviolations-", ".dat"),
+        ("pwghistnorms-", ".top"),
+        ("pwgevents-", ".lhe"),
+        ("pwgcounters-st4-", ".dat"),
+        ("pwgcounters-st3-", ".dat"),
+        ("pwgcounters-st1-", ".dat"),
+        ("pwgcounters-st2-", ".dat"),
+        ("pwg-xg1-btl-", ".top"),
+        ("pwg-xg1-xgrid-btl-", ".dat"),
+        ("pwg-xg1-xgrid-rm-", ".dat"),
+        ("pwg-xg2-xgrid-btl-", ".dat"),
+        ("pwg-xg2-xgrid-rm-", ".dat"),
+        ("pwg-xg2-xgrid-btl-", ".top"),
+        ("pwg-xg2-xgrid-rm-", ".top"),
+        ("pwg-st2-xgrid-btl-", ".top"),
+        ("pwg-st2-xgrid-rm-", ".top"),
+        ("pwgubound-", ".dat"),
+        ("sigborn_equiv-", ""),
+        ("sigvirtual_equiv-", ""),
+        ("sigreal_btl0_equiv-", ""),
+        ("sigreal_rad_equiv-", ""),
+        ("pwgalone-output", ".top"),
+        ("pwgfullgrid-btl-", ".dat"),
+        ("pwgfullgrid-rm-", ".dat"),
+        ("pwhg_checklimits-", ""),
+        ("pwg-", "-NLO.top"),
+        ("pwg-", "-borngrid-stat.dat"),
+        ("pwg-", "-xg1-stat.dat"),
+        ("pwg-", "-xg2-stat.dat"),
+        ("pwg-", "-st3-stat.dat"),
+        ("pwg-", "-st2-stat.dat"),
+    ]:
+        for f in Path(args.path).glob(f"{n}*{s}"):
+            os.remove(f)
 
 
 if __name__ == "__main__":
