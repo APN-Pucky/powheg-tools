60ccfb91b946fd05bacd83812dd5216a2004f165
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2024-03-27 13:25:07 +0100

    More aggressive cleaning

--- powheg_tools/cleanpowheg/__init__.py
+++ powheg_tools/cleanpowheg/__init__.py
@@ -16,72 +16,62 @@ def main():
 
     args = parser.parse_args()
 
+    id = "*"
+
     for r in ["btl", "rm", "reg"]:
         # remove single file
         for f in [
-            "bornequiv",
+            f"bornequiv",
+            f"pwggrid.dat",
+            f"FlavRegList",
+            f"pwgborngrid.top",
+            f"pwgcounters.dat",
+            f"pwghistnorms.top",
+            f"pwgxgrid.dat",
+            f"realequivregions-rad",
+            f"virtequiv",
+            f"parameters.ol",
+            f"pwg-btilde-fullgrid.lock",
+            f"pwg-remn-fullgrid.lock",
             f"pwg-{r}grid.top",
-            "pwggrid.dat",
-            "pwgubound.dat",
             f"realequivregions-{r}",
-            "FlavRegList",
-            "pwgborngrid.top",
-            "pwgcounters.dat",
-            "pwghistnorms.top",
-            "pwgxgrid.dat",
-            "realequivregions-rad",
-            "pwgboundviolations.dat",
-            "pwgevents.lhe",
-            "pwg-stat.dat",
-            "pwhg_checklimits",
-            "virtequiv",
-            "parameters.ol",
             f"mint_upb_{r}upb.top",
             f"mint_upb_{r}upb_rat.top",
-            "pwg-btilde-fullgrid.lock",
-            "sigborn_equiv",
-            "pwg-remn-fullgrid.lock",
             f"mint_upb_{r}upb.top",
             f"pwggrid-{r}.dat",
+            f"pwg{id}-stat.dat",
+            f"pwgxgrid-{r}{id}.dat",
+            f"pwggrid-{r}{id}.dat",
+            f"pwg{r}upb{id}.dat",
+            f"pwgboundviolations{id}.dat",
+            f"pwghistnorms{id}.top",
+            f"pwgevents{id}.lhe",
+            f"pwgcounters-st4{id}.dat",
+            f"pwgcounters-st3{id}.dat",
+            f"pwgcounters-st1{id}.dat",
+            f"pwgcounters-st2{id}.dat",
+            f"pwg-xg1-{r}{id}.top",
+            f"pwg-xg1-xgrid-{r}{id}.dat",
+            f"pwg-xg2-xgrid-{r}{id}.dat",
+            f"pwg-xg2-xgrid-{r}{id}.top",
+            f"pwg-st2-xgrid-{r}{id}.top",
+            f"pwgubound{id}.dat",
+            f"sigborn_equiv{id}",
+            f"sigvirtual_equiv{id}",
+            f"sigreal_{r}0_equiv{id}",
+            f"sigreal_rad_equiv{id}",
+            f"pwgalone-output{id}.top",
+            f"pwgfullgrid-{r}{id}.dat",
+            f"pwhg_checklimits{id}",
+            f"pwg{id}-NLO.top",
+            f"pwg{id}-borngrid-stat.dat",
+            f"pwg{id}-xg1-stat.dat",
+            f"pwg{id}-xg2-stat.dat",
+            f"pwg{id}-st3-stat.dat",
+            f"pwg{id}-st2-stat.dat",
         ]:
-            p = os.path.join(args.path, f)
-            if os.path.exists(p):
-                os.remove(p)
-        # remove parallel run files
-        for n, s in [
-            ("pwg-", "-stat.dat"),
-            (f"pwgxgrid-{r}", ".dat"),
-            (f"pwggrid-{r}-", ".dat"),
-            (f"pwg{r}upb-", ".dat"),
-            ("pwgboundviolations-", ".dat"),
-            ("pwghistnorms-", ".top"),
-            ("pwgevents-", ".lhe"),
-            ("pwgcounters-st4-", ".dat"),
-            ("pwgcounters-st3-", ".dat"),
-            ("pwgcounters-st1-", ".dat"),
-            ("pwgcounters-st2-", ".dat"),
-            (f"pwg-xg1-{r}-", ".top"),
-            (f"pwg-xg1-xgrid-{r}-", ".dat"),
-            (f"pwg-xg2-xgrid-{r}-", ".dat"),
-            (f"pwg-xg2-xgrid-{r}-", ".top"),
-            (f"pwg-st2-xgrid-{r}-", ".top"),
-            ("pwgubound-", ".dat"),
-            ("sigborn_equiv-", ""),
-            ("sigvirtual_equiv-", ""),
-            (f"sigreal_{r}0_equiv-", ""),
-            ("sigreal_rad_equiv-", ""),
-            ("pwgalone-output", ".top"),
-            (f"pwgfullgrid-{r}-", ".dat"),
-            ("pwhg_checklimits-", ""),
-            ("pwg-", "-NLO.top"),
-            ("pwg-", "-borngrid-stat.dat"),
-            ("pwg-", "-xg1-stat.dat"),
-            ("pwg-", "-xg2-stat.dat"),
-            ("pwg-", "-st3-stat.dat"),
-            ("pwg-", "-st2-stat.dat"),
-        ]:
-            for f in Path(args.path).glob(f"{n}*{s}"):
-                os.remove(f)
+            for file in Path(args.path).glob(f"{f}"):
+                os.remove(file)
 
 
 if __name__ == "__main__":
